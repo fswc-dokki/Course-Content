4e309d7f7057e8b49dd23b911c92c78ef68dbaa4
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2019-11-22 14:47:49 +0200

    S13

--- S13/bookListApp/src/components/BookApp.js
+++ S13/bookListApp/src/components/BookApp.js
@@ -1,36 +1,52 @@
 import React, { Component } from "react";
 import BookForm from "./BookForm";
 import BookCardsContainer from "./BookCardsContainer";
+import Store from "../Store";
+
+const store = new Store("books2");
 
 export default class BookApp extends Component {
-  constructor(props) {
-    super(props);
-    this.addBook = this.addBook.bind(this);
-    this.state = {
-      newBook: {
-        title: "",
-        author: "",
-        desc: ""
-      }
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.addBook = this.addBook.bind(this);
+        this.updateData = this.updateData.bind(this);
+        this.state = {
+            newBook: {
+                title: "",
+                author: "",
+                desc: ""
+            },
+            data : store.ShowAll()
+        };
+    }
 
-  addBook(title, author, desc) {
-    this.setState({
-      newBook: {
-        title,
-        author,
-        desc
-      }
-    });
-  }
+    updateData() {
+        this.setState({
+            data: store.ShowAll()
+        });
+    }
 
-  render() {
-    return (
-      <div className="container">
-        <BookForm onSubmit={this.addBook} />
-        <BookCardsContainer newBook={this.state.newBook} />
-      </div>
-    );
-  }
+    addBook(title, author, desc) {
+        if (
+            title !== "" &&
+            author !== ""
+        ) {
+            let newBook = {
+                title,
+                author,
+                desc
+            }
+            store.Add(newBook);
+            this.updateData();
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <BookForm onSubmit={this.addBook} />
+                <BookCardsContainer data={this.state.data} />
+            </div>
+        );
+    }
 }
--- S13/bookListApp/src/components/BookCardsContainer.js
+++ S13/bookListApp/src/components/BookCardsContainer.js
@@ -1,37 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import BookCard from "./BookCard";
-import Store from "../Store";
 
-const store = new Store("books2");
 
-export default class BookCardsContainer extends Component {
-    constructor() {
-        super();
-        this.state = {
-            data: []
-        };
-    }
-
-    componentDidMount() {
-        this.setState({
-            data: store.ShowAll()
-        });
-    }
-
-    componentDidUpdate(props) {
-        // if (props.newBook !== this.props.newBook) {
-        console.log("I'm called");
-        this.setState(prevState => ({
-            data: prevState.data.push(props.newBook)
-        }));
-        store.Add(this.props.newBook);
-        // }
-    }
-    render() {
+export default function BookCardsContainer(props) {
         return (
             <section className="book-cards">
-                {this.state.data &&
-                    this.state.data.map(
+                {props.data &&
+                    props.data.map(
                         (el, idx) =>
                             el && (
                                 <BookCard
@@ -44,5 +19,4 @@ export default class BookCardsContainer extends Component {
                     )}
             </section>
         );
-    }
 }
